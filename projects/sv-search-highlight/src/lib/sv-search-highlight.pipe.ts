97a4166ca1554494a9e519f207111c9c41d0e0e7
=samira.vahidi <=samira.vahidi@rakuten.com>
2021-05-10 01:22:35 +0900

    update readme

--- projects/sv-search-highlight/src/lib/sv-search-highlight.pipe.ts
+++ projects/sv-search-highlight/src/lib/sv-search-highlight.pipe.ts
@@ -4,8 +4,10 @@ import {Pipe, PipeTransform} from '@angular/core';
   name: 'svSearchHighlight'
 })
 export class SvSearchHighlightPipe implements PipeTransform {
-  transform(value: any, search: string): string {
-    if (!value) { return; }
+  transform(value: any, search: string): any {
+    if (!value) {
+      return;
+    }
     const re = new RegExp(search, 'gi');
     const match = value.match(re);
     return search ? value.replace(re, `<mark>${match && match[0]}</mark>`) : value;
